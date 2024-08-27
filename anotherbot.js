5d7c4b392762f699d2c728ca1000511c7d62286b
Leo Calnan <leocalnan@hotmail.co.uk>
2024-08-27 13:53:59 +0100

    minecraft bot starting

--- anotherbot.js
+++ anotherbot.js
@@ -1,12 +1,33 @@
 const mineflayer = require("mineflayer");
 
-const settings = {
+var settings = {
     username: "TestMachine",
     host: "localhost",
     port: 25565,
+    version: '1.19.4'
 };
 
 const bot = mineflayer.createBot(settings);
+
+bot.once("spawn", ()=>{
+    bot.chat("Hello everyone!");
+});
+
+bot.on("move", ()=>{
+    let friend = bot.nearestEntity();
+
+    if (friend) {
+        bot.lookAt(friend.position.offset(0, friend.height, 0));
+    }
+});
+
+var walking = false;
+
+bot.on("entityHurt", (entity)=>{
+    if (entity != bot.entity) return;
+    walking = !walking;
+    bot.setControlState("forward", walking);
+});
 
 bot.on('spawn', () => {
     console.log('Bot has spawned in the game.')
@@ -22,7 +43,7 @@ bot.on('spawn', () => {
     }
   })
 
-async function digDown() {
+  async function digDown() {
     let blockPosition = bot.entity.position.offset(0, -1, 0);
     let block = bot.blockAt(blockPosition);
 
